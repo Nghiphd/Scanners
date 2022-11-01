60bb74f3f9753817a818a99c1d777f2b1e796b58
Nghi Phan <nghi.phan@mrjaffesclass.com>
2022-11-01 11:17:08 -0700

    temp

---
+++ MyScanner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class MyScanner
+{
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        MyScanner myScanner = new MyScanner();
+        myScanner.run();
+    }
+}
+
